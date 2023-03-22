a7dfbd33f32a8a37c2d0f1bb5f3fe9f9cabff650
pviniciussilva <pedroviniciusdesousarochasilva@gmail.com>
2023-03-22 11:50:29 -0300

    exercicios

--- Lista01PYTHON/L1q1.py
+++
@@ -1,3 +0,0 @@
-#faça um programa que mostra a mensagem 'Alo mundo' na tela.
-#saida
-print('Alo mundo')
--- Lista01PYTHON/L1q2.py
+++
@@ -1,7 +0,0 @@
-#Faça um Programa que peça um número e então mostre a mensagem
-#  O número informado foi [número].
-
-numero = input('Informe um número: ')#entrada de dados
-#input entrega um str (string)
-
-print('O numero informado foi', numero)#saída de dados
